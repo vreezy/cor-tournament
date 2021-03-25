a01cffdd707ee5fd2ba163cd7efb95820db129ee
vreezy117 <vreezy117@googlemail.com>
2021-03-25 20:35:04 +0100

    azure Insight and build path added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const appInsights = new ApplicationInsights({ config: {
+   instrumentationKey: '894f90eb-210b-4fc6-9a2a-fdb43901747d'
+   /* ...Other Configuration Options... */
+ } });
+ appInsights.loadAppInsights();
+ appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +21,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
